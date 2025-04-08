068c09ab0e010430c4bb2817ff28a0cc47c1476a
yohanashtekar <yohan.ashtekar@gmail.com>
2025-04-08 02:20:50 -0500

    editing logging

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -74,6 +74,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
+        app.logger.info('Successful login.')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
